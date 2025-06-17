94ca592bb3cfef30addfd90923c12f9d23f0c347
Marcelo Cho <marcelo.cho@gmail.com>
2025-06-17 10:20:34 -0300

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+const EleventyDevServer = require("@11ty/eleventy-dev-server");
+
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/admin");
+
+
+  eleventyConfig.setServerOptions({
+    middleware: [
+        function (req, res, next) {
+            if (req.statusCode === 404) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(require("fs").readFileSync("_site/404.html"));
+            } else {
+                next();
+            }
+        }
+    ]
+});
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
+
